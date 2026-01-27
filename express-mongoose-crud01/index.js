d8623836b5a149809fe6f5f6443d58128cfc95e0
aslamsm <aslamsm@yahoo.com>
2026-01-27 16:48:18 +0530

    Set up Express

--- express-mongoose-crud01/index.js
+++ express-mongoose-crud01/index.js
@@ -1,1 +1,22 @@
 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+// Routes
+const customerRoutes = require("./routes/items.routes");
+app.use("/customers", customerRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

251e7656c0dedb593006e39d7387347b4f04a91d
aslamsm <aslamsm@yahoo.com>
2026-01-27 18:26:38 +0530

    Express server with MongoDB

--- express-mongoose-crud02/index.js
+++ express-mongoose-crud02/index.js
@@ -1,1 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+// Routes
+const customerRoutes = require("./routes/items.routes");
+app.use("/items", customerRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

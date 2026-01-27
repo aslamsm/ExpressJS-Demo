1612cec651e5ec96d5368f9ba26c5797ce6df278
aslamsm <aslamsm@yahoo.com>
2026-01-27 18:23:48 +0530

    model with schema

--- express-mongoose-crud02/models/Item.js
+++ express-mongoose-crud02/models/Item.js
@@ -1,1 +1,18 @@
 
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rate: Number,
+    unit: { type: String, required: true },
+    isTaxable: { type: Boolean, default: false },
+    taxPercentage: Number,
+    isActive: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Item", itemSchema);

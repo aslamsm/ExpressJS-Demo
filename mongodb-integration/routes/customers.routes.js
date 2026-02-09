83028c1432349f2e4c890595c1a46cbc489797ef
aslamsm <aslamsm@yahoo.com>
2026-02-09 17:41:04 +0300

    Implement CRUD operations for customers

--- mongodb-integration/routes/customers.routes.js
+++ mongodb-integration/routes/customers.routes.js
@@ -1,1 +1,90 @@
+const express = require("express");
+const router = express.Router();
+const Customer = require("../models/Customer");
+const validateCustomer = require("../middlewares/validateCustomer");
+
+router.post("/", validateCustomer, async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.status(201).json(customer);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.params.id);
+
+    if (!customer) {
+      return res.status(404).json({
+        message: "Customer not found",
+      });
+    }
+
+    res.status(200).json(customer);
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid Customer ID",
+    });
+  }
+});
+
+router.put("/:id", validateCustomer, async (req, res) => {
+  try {
+    const updatedCustomer = await Customer.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedCustomer) {
+      return res.status(404).json({
+        message: "Customer not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Customer updated successfully",
+      data: updatedCustomer,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to update customer",
+      error: error.message,
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCustomer = await Customer.findByIdAndDelete(req.params.id);
+
+    if (!deletedCustomer) {
+      return res.status(404).json({
+        message: "Customer not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Customer deleted successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid customer ID",
+    });
+  }
+});
+
+module.exports = router;
 

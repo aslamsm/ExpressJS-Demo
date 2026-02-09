bafe24983dbb7b9d91c744abc737b168aed2d767
aslamsm <aslamsm@yahoo.com>
2026-02-09 17:38:31 +0300

    customer schema

--- mongodb-integration/models/Customer.js
+++ mongodb-integration/models/Customer.js
@@ -1,1 +1,33 @@
+// Import the mongoose library to interact with MongoDB
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    creditLimit: {
+      type: Number,
+      required: true,
+      min: 10000,
+      max: 1000000,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Customer", customerSchema);
 

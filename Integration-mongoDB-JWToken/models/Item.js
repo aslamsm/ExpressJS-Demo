5f0ba12a54e38334bf23eee8bc8a250c21d16c4a
aslamsm <aslamsm@yahoo.com>
2026-02-14 07:35:22 +0300

    Add Item model with schema and timestamps

--- Integration-mongoDB-JWToken/models/Item.js
+++ Integration-mongoDB-JWToken/models/Item.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rate: Number,
+    unit: { type: String, required: true },
+    isTaxable: { type: Boolean, default: false },
+    taxPercentage: Number,
+    isActive: { type: Boolean, default: true },
+  },
+  // Enable automatic timestamps (createdAt and updatedAt fields)
+  { timestamps: true },
+);
+module.exports = mongoose.model("Item", itemSchema);
 
